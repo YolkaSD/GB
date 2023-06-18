7dc8a7caa6a58f491f00c5f07a11656e4332b6db
Slava Rybkin <yolka.sd@gmail.com>
2023-06-18 11:34:03 +0700

    lectureapi lec4

---
+++ src/main/java/org/example/lectureapi/lec5/Main.java
@@ -0,0 +1,33 @@
+package org.example.lectureapi.lec5;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+//        Map<Integer, String> db = new HashMap<>();
+//        db.putIfAbsent(1, "Один");
+//        db.put(2, "два");
+//        db.put(null, "!null");
+//        System.out.println(db);
+//        System.out.println(db.get(44));
+//        db.remove(null);
+//        System.out.println(db);
+//        System.out.println(db.keySet());
+//        System.out.println(db.values());
+//        Map<Integer, String> db = new HashMap<>();
+//        db.put(1, "Один");
+//        db.put(2, "Два");
+//        db.put(3, "Три");
+//
+//        for (Map.Entry<Integer, String> item: db.entrySet()) {
+//            System.out.printf("[%d: %s] \n", item.getKey(), item.getValue());
+//        }
+
+        Map<Integer, String> treeDb = new TreeMap<>();
+        Map<Integer, String> linkedDb = new LinkedHashMap<>();
+
+    }
+}
