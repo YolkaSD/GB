6d8a777a9a2b73cea83c4bbaee71e4ea15266ad1
Slava Rybkin <yolka.sd@gmail.com>
2023-06-23 17:17:35 +0700

    lectureapi lec6

---
+++ src/main/java/org/example/lectureapi/lec6/Main.java
@@ -0,0 +1,24 @@
+package org.example.lectureapi.lec6;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+//        var a = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+//        var b = new HashSet<>(Arrays.asList(2, 3, 4, 5, 7, 11, 26, 17));
+//        var u = new HashSet<>(a);
+//        u.addAll(b);
+//        var r = new HashSet<>(a);
+//        r.retainAll(b);
+//        var s = new HashSet<>(a);
+//        r.retainAll(b);
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(u);
+//        System.out.println(r);
+//        System.out.println(s);
+
+
+    }
+}
